c2e048323bd987d2085b5e5c8032112c94d3b32b
Mark Duarte <duarte.mark8@gmail.com>
2023-03-29 11:19:49 -0700

    updated server.js file to get process.env.PORT

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
-const { PORT = 5001 } = process.env;
+// const { PORT = 5001 } = process.env;
 
-const app = require("./app");
-const listener = () => console.log(`Listening on Port ${PORT}!`);
-app.listen(PORT, listener);
+// const app = require("./app");
+// const listener = () => console.log(`Listening on Port ${PORT}!`);
+// app.listen(PORT, listener);
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 5001;
+
+router.get("/", cors(), (req, res) => {
+  res.json({ message: "Hello Beep Boop Bops!" });
+});
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT} `);
+});
+
+module.exports = app;
